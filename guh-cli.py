d408a323416974259bca3ee0bb4edc50358d4640
Simon Stürz <stuerz.simon@gmail.com>
2015-02-12 21:29:36 +0100

    added license headers

--- guh-cli.py
+++ guh-cli.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#                                                                         #
+#  Copyright (C) 2015 guh                                                 #
+#                                                                         #
+#  This file is part of guh-cli.                                          #
+#                                                                         #
+#  guh-cli is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by   #
+#  the Free Software Foundation, version 2 of the License.                #
+#                                                                         #
+#  guh-cli is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the           #
+#  GNU General Public License for more details.                           #
+#                                                                         #
+#  You should have received a copy of the GNU General Public License      #
+#  along with guh. If not, see <http://www.gnu.org/licenses/>.            #
+#                                                                         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
 import sys
 import telnetlib
 import json
